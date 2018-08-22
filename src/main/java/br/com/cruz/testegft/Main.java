29ad1ab5ce41bfe3846ece35c744d87a1227e694
Carlos Cruz <Carlos Cruz@192.168.1.34>
2018-08-22 16:17:23 -0300

    Commit Final

--- src/main/java/br/com/cruz/testegft/Main.java
+++ src/main/java/br/com/cruz/testegft/Main.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 import br.com.cruz.testegft.services.PedidoService;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	PedidoService pedidoService = new PedidoService();
+public class Main {
+
+	public static void main(String[] args) {
+		PedidoService pedidoService = new PedidoService();
     	Scanner scan = new Scanner(System.in);
     	System.out.print("Informe o pedido: ");
     	String pedido = scan.nextLine();
     	System.out.println(pedidoService.realizarPedido(pedido));
     	scan.close();
-    }
+	}
+
 }
